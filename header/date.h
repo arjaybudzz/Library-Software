373930f92d4cc2010c33a34af75aaf338c70bfa0
arjaybudzz <archessonan.1@gmail.com>
2024-02-05 22:00:06 +0800

    declared Date class

---
+++ header/date.h
@@ -0,0 +1,30 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+#include "month.h"
+#include "day.h"
+#include "year.h"
+
+class Date {
+    public:
+        Date();
+        Date(const Month&, const Day&, const Year&);
+
+        // setters and getters
+
+        void set_month(const Month&);
+        void set_day(const Day&);
+        void set_year(const Year&);
+
+        Month get_month() const;
+        Day get_day() const;
+        Year get_year() const;
+
+    private:
+        Month month;
+        Day day;
+        Year year;
+};
+
+#endif
