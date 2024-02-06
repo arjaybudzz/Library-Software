2f9ddcc05149f1506b0c3d8c954c7008e2a05da5
arjaybudzz <archessonan.1@gmail.com>
2024-02-06 14:28:17 +0800

    implemented name i/o operations

--- header/book.h
+++ header/book.h
@@ -3,12 +3,15 @@
 
 #include <iostream>
 #include "date.h"
+#include "name.h"
 
 class Book {
     public:
-
+        Book();
+        Book(const std::string&, const Name&, const Date&);
     private:
-        std::string author;
+        std::string title;
+        Name author;
         Date published;
 };
 
