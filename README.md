9fccd6f613ed676e924c955cf19134e81b333888
arjaybudzz <archessonan.1@gmail.com>
2024-02-07 08:22:36 +0800

    modified README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Library-Software
+
+## Functionalities
+
+1. User can search books.
+2. User can borrow books.
+3. User can see the available books.
--- src/book.cpp
+++ src/book.cpp
@@ -1,3 +1,8 @@
 #include "../header/book.h"
 #include <iostream>
 
+Book::Book():
+    title{}, author{}, published{} {}
+
+Book::Book(const std::string& t_val, const Name& n_val, const Date& d_val):
+    title{t_val}, author{n_val}, published{d_val} {}
